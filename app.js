f72d7ae267f11fd3b7628d17a462093693c742a7
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-11-05 17:44:18 +0100

    Started working on views

--- app.js
+++ app.js
@@ -15,18 +15,23 @@ app.set("view engine", "ejs")
 app.set("views", "./views")
 
 app.use(express.urlencoded({extended: false}))
+app.use("/", express.static(__dirname + "/public"))
 
+// EJS Views
 app.get("/", (req, res) => {
-    res.redirect("/blog")
+    res.render("home")
+})
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+app.get("/signin", (req, res) => {
+    res.render("signin")
 })
 
+// API Routes
 app.use("/auth", authRouter)
 app.use("/blog", blogRouter)
-app.use(
-    "/writer",
-    authenticate,
-    writerRouter
-)
+app.use("/writer", authenticate, writerRouter)
 
 // Handle unknown routes
 app.use((req, res, next) => {
