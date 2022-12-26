a89671c3dd98bad599e7870a24ae951d237979a4
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-12-26 15:50:47 +0100

    Moved auth middlewares to controllers file

---
+++ controllers/auth.controller.js
@@ -0,0 +1,85 @@
+require("dotenv").config()
+
+const jwt = require("jsonwebtoken")
+
+const User = require("../models/user")
+
+async function signupUser(req, res) {
+    const userDetails = req.body
+    const { first_name, last_name, email, password } = userDetails
+    if (!first_name || !last_name || !email || !password) {
+        return res.status(400).send({
+            message: "Please enter all the required info"
+        })
+    }
+
+    const user = new User({ ...userDetails })
+
+    // Catch validation errors
+    try {
+        await user.validate()
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+
+    // Catch other errors 
+    try {
+        await user.save()
+
+        return res.send({
+            message: "User created successfully",
+            user
+        })
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+}
+
+
+async function signinUser(req, res) {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).send({
+            message: "Both email and password must be entered"
+        })
+    }
+
+    const user = await User.findOne({
+        email
+    }).exec()
+
+    if (!user) {
+        return res.status(404).send({
+            message: "User with that email not found."
+        })
+    }
+    const passwordIsValid = await user.validatePassword(password)
+
+    if (passwordIsValid) {
+        const payload = {
+            id: user._id
+        }
+        const JWT_SECRET = process.env.JWT_SECRET
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" })
+
+        return res.send({
+            message: "You've signed in successfully",
+            token
+        })
+    } else {
+        return res.status(401).send({
+            message: "Your password is incorrect! Try again"
+        })
+    }
+
+}
+
+module.exports = {
+    signupUser,
+    signinUser
+}
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,86 +1,12 @@
-require("dotenv").config()
-
 const express = require("express")
 const authRouter = express.Router()
 
-const jwt = require("jsonwebtoken")
-
-const User = require("../models/user")
+const { signupUser, signinUser }  = require("../controllers/auth.controller.js")
 
 
-authRouter.post("/signup", async (req, res) => {
-    const userDetails = req.body
-    const { first_name, last_name, email, password } = userDetails 
-    if (!first_name || !last_name || !email || !password) {
-        return res.status(400).send({
-            message: "Please enter all the required info"
-        })
-    }
-    
-    const user = new User({...userDetails})
+authRouter.post("/signup", signupUser)
 
-    // Catch validation errors
-    try {
-        await user.validate()
-    }
-    catch (err) {
-        return res.status(400).send(err)
-    }
-
-    // Catch other errors 
-    try {
-        await user.save()
-
-        return res.send({
-            message: "User created successfully",
-            user
-        })
-        
-    } catch (err) {
-        res.status(500).send(err)
-    }
-
-})
-
-
-authRouter.post("/signin", async (req, res) => {
-    const { email, password } = req.body
-
-    if (!email || !password) {
-        return res.status(400).send({
-            message: "Both email and password must be entered"
-        })
-    }
-
-    const user = await User.findOne({
-        email
-    }).exec()
-
-    if (!user) {
-        return res.status(404).send({
-            message: "User with that email not found."
-        })
-    }
-    const passwordIsValid = await user.validatePassword(password)
-
-    if (passwordIsValid) {
-        const payload = {
-            id: user._id
-        }
-        const JWT_SECRET = process.env.JWT_SECRET
-        const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "1h"})
-
-        return res.send({
-            message: "You've signed in successfully",
-            token
-        })
-    } else {
-        return res.status(401).send({
-            message: "Your password is incorrect! Try again"
-        })
-    }
-
-})
+authRouter.post("/signin", signinUser)
 
 
 module.exports = authRouter
